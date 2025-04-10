3b071b004c0b233a16aceaa908679ea9ded3e189
Chauhan Dhruvil <chauhandhruvil12@gmail.com>
2025-04-10 17:27:51 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,24 +1,28 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  userName: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    default: "user",
-  },
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        default: "user",
+    },
+    token: {
+        type: String, // 👈 Add this field
+        default: null,
+    },
 });
 
 const User = mongoose.model("User", UserSchema);
