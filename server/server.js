98680e77d43efc87c38ace8649c3d76c46226612
Chauhan Dhruvil <chauhandhruvil12@gmail.com>
2025-04-10 15:12:13 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173", // update with frontend domain in production
+        origin: "*", // update with frontend domain in production
         methods: ["GET", "POST"],
     },
 });
